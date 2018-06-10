13632d45a2a94a1db7008e182cc10e897e6648e7
Yan Charnyy <ycharnyy@gmail.com>
2018-06-10 16:45:21 +0300

    3 задача первого урока, подглядывал к решению Ильи-2, а решение с regex пока совсем не очевидное

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -6,6 +6,38 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
+    let numbers = [];
+    numbers = findNumber(str);
+    numbers.sort((a, b) => a - b);
+
+    let answ = {
+        min: numbers[0],
+        max: numbers[numbers.length-1]
+    }
+
+    return answ;
+
+
+    function findNumber(str) {
+        let numbers = [],
+            fnumber;
+        while (str != '') {
+            fnumber = parseFloat(str);
+
+            if (str[0] === " " || str[0] === "+") {
+                str = str.slice(1)
+
+            } else if (!(isNaN(fnumber))) {
+                numbers.push(fnumber);
+                console.log(fnumber);
+                str = str.slice(fnumber.toString().length);
+
+            } else {
+                str = str.slice(1);
+            }
+
+        }
+        return numbers
+    }
 
 }
-
