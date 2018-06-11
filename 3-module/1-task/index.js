7abb8c748922c7109e6f41827dbfbea7a9b98980
Yan Charnyy <ycharnyy@gmail.com>
2018-06-12 00:39:08 +0300

    3 блок, 2 задача не очень пошла

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,7 +4,19 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
+
+// Как правильно избавиться от последнего обрезания \n ?
+
 function showSalary(data, age) {
+    dataNew = data.filter(function(item) {
+        return item.age<=age
+    });
+
+    result = dataNew.reduce( function(string,item) {
+        return string + item.name + ', ' + item.balance +'\n'
+    }, '' )
+
+    result = result.slice(0,-1);
+
+    return result
 }
-
-
--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,6 +4,42 @@ let calendar = {
 };
 
 calendar[Symbol.iterator] = function () {
-};
 
+
+    let current = this.from;
+    let last = this.to;
+
+    current.setDate(current.getDate() + 1);
+    last.setDate(last.getDate() + 1);
+
+    // метод должен вернуть объект с методом next()
+    return {
+        next() {
+            if (current <= last) {
+
+                checkweekend = current.getDay();
+                dayM = current.getDate();
+                current.setDate(current.getDate() + 1);
+
+                if (dayM <= 9) {dayM='0'+dayM}
+                if ((checkweekend === 0) || (checkweekend === 6)) {
+                    answer = '['+dayM+']'
+                } else {
+                    answer = dayM.toString();
+                }
+
+                return {
+                    done: false,
+                    value: answer
+                };
+            } else {
+                return {
+                    done: true
+                };
+            }
+        }
+
+    }
+
+};
 
