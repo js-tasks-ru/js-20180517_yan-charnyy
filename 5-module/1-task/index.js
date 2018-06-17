357f348e1a273e95e659a2f74d97e7618d2a4c7d
Yan Charnyy <ycharnyy@gmail.com>
2018-06-17 20:08:56 +0300

    5 модуль 1 задача, есть вопросы как правильно все же работать с колонками в таблицах, как их лучше определять заранее

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -5,4 +5,20 @@
  * @param {Element} table
  */
 function highlight(table) {
+
+    let allTr = table.querySelector('tbody').getElementsByTagName('tr');
+    const GENDERCOLUMN = 3, AGECOLUMN = 2;
+
+    for (let item of allTr) {
+        if (item.querySelector('td:last-child').getAttribute('data-available') === 'true') {
+            item.classList.add('available')
+        }else if (item.querySelector('td:last-child').getAttribute('data-available') === 'false') {
+            item.classList.add('unavailable')
+        }else {item.hidden = true}
+
+        item.querySelector(`td:nth-child(${GENDERCOLUMN})`).innerHTML === 'm' ? item.classList.add('male'):item.classList.add('female');
+        item.querySelector(`td:nth-child(${AGECOLUMN})`).innerHTML < 19 ? item.style="text-decoration: line-through": null;
+
+    }
+
 }
